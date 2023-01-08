eaad37f0667aba5d0058c19db4a2121a3ebf540f
tylerhilton3 <tyler.hilton@sbcglobal.net>
2023-01-07 23:27:12 -0700

    1/7 hw

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    static void Main(string[] args) {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != magicNumber) {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (magicNumber > guess) {
+                Console.WriteLine("Higher");
+            } else if (magicNumber < guess) {
+                Console.WriteLine("Lower");
+            } else {
+                Console.WriteLine("You guessed it!");
+            }
+        }                    
     }
 }
